224af3c8e83d6f8ec48ccca320910cc6a0bc39be
vasu16mstp <pediredlavasudha@gmail.com>
2023-12-05 18:43:46 -0500

    Initial commit

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1909903.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 158445000, 158445001, 158445024, 158445004, 158445005, 158445006, 158445007, 158445008, 158445010, 158445011, 158445012, 158445013, 158445015, 158445016, 158765787, 159100837, 159100854, 159231774, 159286554, 159290088, 159407785, 159536934, 159584534, 159586763 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/158445021/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/158445022/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/158445020/1/ammo.js', 'preload' : true},
+];
